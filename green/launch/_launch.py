8380ce6d84109de51eafba148e0832cbabcdd29c
MohZak <kokoriquaye@gmail.com>
2023-11-03 20:57:12 -0400

    added parameters

--- green/launch/_launch.py
+++ green/launch/_launch.py
@@ -1,14 +1,28 @@
 import launch
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument as DLA
+from launch.substitutions import LaunchConfiguration as LC
 
 def generate_launch_description():
+    banana = Node(
+        package='banana',
+        executable='webcam_driver_pub',
+        name='webcam_driver'
+    )
+    green = Node(
+        package='green',
+        executable='color_filter',
+        name='color_filter',
+        output='screen',
+        parameters=[
+            {
+                "param_hi": [255, 255, 255],
+                "param_lo": [200, 200, 200],
+                "desired_co": [255, 0, 0]
+            }
+            ]
+        )
     return launch.LaunchDescription([
-        Node(
-            package='banana',
-            executable='webcam_driver_pub',
-            name='webcam_driver'),
-        Node(
-            package='green',
-            executable='color_filter',
-            name='color_filter'),
-])
+        banana,
+        green,
+    ])
--- green/src/color_filter.cpp
+++ green/src/color_filter.cpp
@@ -4,6 +4,8 @@
 #include <cv_bridge/cv_bridge.h>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "rcl_interfaces/msg/parameter_event.hpp"
+
 using std::placeholders::_1;
 
 /**
@@ -13,55 +15,74 @@ using std::placeholders::_1;
 
 class WebcamSubscriber : public rclcpp::Node
 {
-  public:
-    WebcamSubscriber()
-    : Node("Webcam_Subscriber")
-    {
-		// Create Subscriber
-		subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-				"/webcam/image_raw", 10, std::bind(&WebcamSubscriber::image_processor, this, _1));
-    }
+	public:
+		WebcamSubscriber()
+			: Node("Webcam_Subscriber")
+		{
+			//Declare parameters for colors
+			this->declare_parameter<std::vector<long>>("param_hi", {255, 255, 255});
+			this->declare_parameter<std::vector<long>>("param_lo", {200, 200, 200});
+			this->declare_parameter<std::vector<long>>("desired_co", {0, 0, 255});
 
-  private:
-    void image_processor(const sensor_msgs::msg::Image::SharedPtr msg)
-    {
-		//Create CvBridge
-		cv_bridge::CvImagePtr cv_ptr;
-		
-		//Convert message to openCV formate
-		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+			// Create Subscriber
+			subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+					"/webcam/image_raw", 10,std::bind(&WebcamSubscriber::image_processor, this, _1));
 
-		cv::Mat cv_image = cv_ptr->image;
-		
-		//Change color of white objects to red in image
-		for (int i = 0; i < cv_image.rows; i++) {
-			for (int j =0; j < cv_image.cols; j++) {
-				cv::Vec3b& pixel = cv_image.at<cv::Vec3b>(i, j);
-				if (pixel[0] > 220 && pixel[1] > 220 && pixel[2] > 220){
-					pixel[0] = 0;
-					pixel[1] = 0;
-					pixel[2] = 255;
+		}
+	void image_processor(const sensor_msgs::msg::Image::SharedPtr msg)
+		{
+			//Create CvBridge
+			cv_bridge::CvImagePtr cv_ptr;
+
+			//Convert message to openCV formate
+			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+			cv::Mat cv_image = cv_ptr->image;
+
+			//Get parameter values
+			std::vector<long>my_param_hi = this->get_parameter("param_hi").as_integer_array();
+			std::vector<long>my_param_lo = this->get_parameter("param_lo").as_integer_array();
+			std::vector<long>my_param_de = this->get_parameter("desired_co").as_integer_array();
+
+			//Set parameters back to default value when they are changed externally
+			std::vector<long> default_hi = {255, 255, 255};
+			std::vector<long> default_lo = {200, 200, 200};
+			//std::vector<long> default_co = {0, 0, 255};
+			this->set_parameters({rclcpp::Parameter("param_hi", default_hi)});
+			this->set_parameters({rclcpp::Parameter("param_lo", default_lo)});
+			//this->set_parameters({rclcpp::Parameter("desired_co", default_co)});
+
+			//Change color of white objects to red in image
+			for (int i = 0; i < cv_image.rows; i++) {
+				for (int j =0; j < cv_image.cols; j++) {
+					cv::Vec3b& pixel = cv_image.at<cv::Vec3b>(i, j);
+					if (pixel[0] > my_param_lo[0] && pixel[1] > my_param_lo[1] && pixel[2] > my_param_lo[2]){
+						pixel[0] = my_param_de[0];
+						pixel[1] = my_param_de[1];
+						pixel[2] = my_param_de[2];
+
+					}
 				}
 			}
+
+			//Convert image back to ros2 message
+			msg_ = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", cv_image).toImageMsg();
+
+			RCLCPP_INFO(this->get_logger(), "I heard");
+
+			publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/webcam/image_raw_2", 10);
+			publisher_->publish(*msg_);
 		}
-		
-		//Convert image back to ros2 message
-		msg_ = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", cv_image).toImageMsg();
-
-		RCLCPP_INFO(this->get_logger(), "I heard");
-
-		publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/webcam/image_raw_2", 10);
-		publisher_->publish(*msg_);
-    }
-	sensor_msgs::msg::Image::SharedPtr msg_;
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+		sensor_msgs::msg::Image::SharedPtr msg_;
+		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+		rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr parameter_event_sub_;
 };
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<WebcamSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<WebcamSubscriber>());
+	rclcpp::shutdown();
+	return 0;
 }
